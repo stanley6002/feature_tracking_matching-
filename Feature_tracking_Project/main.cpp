0bda8c09db3ced21cb4eb45e73705b743ec22a26
Chih-Hsiang Chang <admin@example.com>
2015-07-11 18:42:13 -0700

    Initial Commit

---
+++ Feature_tracking_Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Feature_tracking_Project
+//
+//  Created by C-HChang on 7/11/15.
+//  Copyright (c) 2015 C-HChang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
